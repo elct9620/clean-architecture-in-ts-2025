fcd0627fdec5b5fe1e53bb3d0a4af752bbe9ecfd
Aotokitsuruya <contact@aotoki.me>
2025-02-23 16:33:12 +0800

    feat: Add Hono with tsyringe dependency injection support

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { tsyringe } from "@hono/tsyringe";
+import { Hono } from "hono";
+import "reflect-metadata";
+
+const app = new Hono();
+
+app.use(async (c, next) => {
+	await tsyringe((container) => {})(c, next);
+});
+
+app.get("/", (c) => c.text("Hello World!"));
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
-	},
+	fetch: app.fetch,
 } satisfies ExportedHandler<Env>;
